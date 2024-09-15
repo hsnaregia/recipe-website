9b6e870f24d71e8943d13890fe89af71a5b40bc0
hsnaregia <139281016+hsnaregia@users.noreply.github.com>
2024-09-15 05:56:17 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    colors:{
+      '--bkg':'#F9F2E7',
+      '--text':'#130B08',
+      '--btn':'#B4452C',
+      '--box':'white',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
