aa42ef7e5f4ad465fe9463c0520610aae61f5fb4
hsnaregia <139281016+hsnaregia@users.noreply.github.com>
2024-09-15 19:02:51 +0330

    first commit

--- src/script.js
+++ src/script.js
@@ -1,26 +1,21 @@
-const url = 'https://www.themealdb.com/api/json/v1/1/search.php?';
-
+const url = "https://www.themealdb.com/api/json/v1/1/search.php?";
 
 function fetch_data(url) {
-    fetch(url)
-  .then(response => response.json())
-  .then(data => console.log(data));
-
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 }
 
-function menu(key){
-    switch (key) {
-        case value:
-            
-        
-        break;
-        case value:
+function menu(key) {
+  switch (key) {
+    case value:
+      break;
+    case value:
+      break;
 
-        break;
-
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 }
 
 fetch_data(url);
